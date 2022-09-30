b30e6d858db81e10c7b4b41ad24c5be769e72b3a
LucreziaC <37901017+LucreziaC@users.noreply.github.com>
2022-09-30 17:18:24 +0200

    added MealItem.js
    upgraded MealsOverviewScreen with MealItemComponent

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,15 +1,40 @@
-import { TabRouter } from '@react-navigation/native';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 
 import {MEALS} from '../data/dummy-data'
+import MealItem from '../components/MealItem';
 
 function MealsOverviewScreen({ route }){
+
+//    const route = useRoute() //alternative to route prop
+
     const catID = route.params.categoryId; // extract the categoryId from categoriesSCreen 
+
+    const displayedMeals= MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(catID) >=0;  //Se c'è almento un categoria nell'array delle categorie che corrisponde a catId
+    });
+
+    function renderMealtem(itemData){
+        const item = itemData.item
+        const mealItemProps={
+            title:item.title,
+            imageUrl: item.imageUrl,
+            affordability: item.affordability,
+            complexity: item.complexity,
+            duration: item.duration
+
+
+        }
+        return (<MealItem {...mealItemProps} />);
+    }
+
     return(
         <View style={styles.container}>
-            <Text>Meals Overview SCreen - { catID }</Text>
+            <FlatList data={displayedMeals} 
+            keyExtractor={(item)=> item.id} 
+            renderItem={renderMealtem}/>
         </View>
-    )
+    );
 }
 
 export default MealsOverviewScreen;
