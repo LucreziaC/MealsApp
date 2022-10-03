6ddc7257ecb1c803144f359cee4756f99017beea
LucreziaC <37901017+LucreziaC@users.noreply.github.com>
2022-10-03 10:51:07 +0200

    setting page option dinamically

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import CategoriesScreen from './screens/CategoriesScreen';
-import MealsOverviewScreen from './screens/MealsOverviewScreen';
+import CategoriesScreen from "./screens/CategoriesScreen";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <><StatusBar style='dark' />
+    <>
+      <StatusBar style="light" />
       <NavigationContainer>
-        <Stack.Navigator screenOptions={ {headerStyle:{backgroundColor: '#680442'}, headerTintColor:'white', contentStyle:{backgroundColor:'#2e011d'}}}>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} title={'All Categories'}/>
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} title={'Meal List'} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#680442" },
+            headerTintColor: "white",
+            contentStyle: { backgroundColor: "#2e011d" },
+          }}
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            title={"All Categories"}
+          />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
+            title={"Meal List"}
+            /* options={({ route, navigation }) => {
+              const catId = route.params.categoryId;
+              return {
+                title: catId
+              };
+            }} */
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
@@ -22,7 +43,5 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-
-  },
+  container: {},
 });
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,47 +1,65 @@
-import { View, FlatList, StyleSheet } from 'react-native';
-import { useRoute } from '@react-navigation/native';
+import { View, FlatList, StyleSheet } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { useLayoutEffect } from "react";
 
-import {MEALS} from '../data/dummy-data'
-import MealItem from '../components/MealItem';
+import { MEALS, CATEGORIES } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
-function MealsOverviewScreen({ route }){
+function MealsOverviewScreen({ route, navigation }) {
+  //    const route = useRoute() //alternative to route prop
 
-//    const route = useRoute() //alternative to route prop
+  const catID = route.params.categoryId; // extract the categoryId from categoriesSCreen
 
-    const catID = route.params.categoryId; // extract the categoryId from categoriesSCreen 
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catID) >= 0; //Se c'è almento un categoria nell'array delle categorie che corrisponde a catId
+  });
 
-    const displayedMeals= MEALS.filter((mealItem) => {
-        return mealItem.categoryIds.indexOf(catID) >=0;  //Se c'è almento un categoria nell'array delle categorie che corrisponde a catId
-    });
+  /* useEffect(() => { //executed after page is rendered
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catID
+    ).title;
 
-    function renderMealtem(itemData){
-        const item = itemData.item
-        const mealItemProps={
-            title:item.title,
-            imageUrl: item.imageUrl,
-            affordability: item.affordability,
-            complexity: item.complexity,
-            duration: item.duration
+    navigation.setOptions({ title: categoryTitle });
+  }, [catID, navigation]);
+ */
 
+  useLayoutEffect(() => {
+    //executed after page is rendered
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catID
+    ).title;
 
-        }
-        return (<MealItem {...mealItemProps} />);
-    }
+    navigation.setOptions({ title: categoryTitle });
+  }, [catID, navigation]);
 
-    return(
-        <View style={styles.container}>
-            <FlatList data={displayedMeals} 
-            keyExtractor={(item)=> item.id} 
-            renderItem={renderMealtem}/>
-        </View>
-    );
+  function renderMealtem(itemData) {
+    const item = itemData.item;
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealtem}
+      />
+    </View>
+  );
 }
 
 export default MealsOverviewScreen;
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        padding:16
-    }
-})
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
